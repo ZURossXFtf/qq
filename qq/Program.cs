154db18abf56047d34d4555b2e7c1fe649450afd
vanish <vanish@DESKTOP-5NO7TTS>
2022-09-19 16:50:37 +0200

    Добавьте файлы проекта.

---
+++ qq/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+try
+{
+    Console.Write("Введите x: ");
+    double x = double.Parse(Console.ReadLine());
+    double res = Math.Sin(Math.Sqrt(x + 1)) - Math.Sin(Math.Sqrt(x - 1));
+    Console.WriteLine($"{res:F2}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.ToString());
+}
